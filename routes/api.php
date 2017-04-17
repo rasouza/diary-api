41a2c469236234c36036e45031e8d736d1ae3586
R. A. Souza <alves.wm@gmail.com>
2017-04-17 18:18:40 -0300

    change OAuth routes to Web router and added Twitter endpoints

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('oauth/github', 'GithubController@redirectToProvider');
-Route::get('oauth/github/callback', 'GithubController@handleProviderCallback');
-
 Route::post('/github/repos', 'GithubController@repos');
+Route::post('/twitter/status', 'TwitterController@tweet');
+Route::post('/twitter/user', 'TwitterController@user');
 
 Route::resource('stories', 'StoryController');
